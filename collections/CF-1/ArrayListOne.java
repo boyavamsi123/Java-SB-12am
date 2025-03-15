ff1051ecac1ae53f7c69c5f065035cbc853929fa
boyavamsi123 <vamvamshi56@gmail.com>
2025-03-15 19:16:15 +0530

    Enjoy

---
+++ collections/CF-1/ArrayListOne.java
@@ -0,0 +1,13 @@
+package com.vamsi.collectins;
+import java.util.ArrayList;
+
+class ArrayListOne{
+    public static void main(String[] args){
+        ArrayList al = new ArrayList();
+        al.add(101);
+        al.add("Rahul");
+        al.add(45000.45);
+        al.add("Delhi");
+        System.out.println(al);
+    }
+}
---
+++ collections/class-2/Array.java
@@ -0,0 +1,10 @@
+public class Array{
+    public static void main(String[] args) {
+        int[] size ={38,40,44,46};
+        //Array are fixed in size .
+        //Array hold only homogeneous .
+
+        //Array hold primitive and object .
+        Employee[] e1 = new Employee[4]; 
+    }
+}
