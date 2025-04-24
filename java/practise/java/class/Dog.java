0d36e73a684139ddac806623742c0953af3e4500
boyavamsi123 <vamvamshi56@gmail.com>
2025-04-24 17:53:46 +0530

    check

---
+++ java/practise/java/class/Dog.java
@@ -0,0 +1,13 @@
+package com.hyrtutorials.telugu;
+
+public class Dog {
+    public String bread = "German Shepard";
+    public int height = 30;
+    public int weight = 35;
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        Dog dog = new Dog();
+        System.out.println(dog.bread);
+    }
+}
